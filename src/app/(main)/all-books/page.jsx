f25f87891635733c2018fe9099f2aa29fbe408e9
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-05-02 00:46:24 +0600

    add loding spinner

--- src/app/(main)/all-books/page.jsx
+++ src/app/(main)/all-books/page.jsx
@@ -5,7 +5,7 @@ const AllBooksPage = async() => {
   const books = await API();
   return (
     <div>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 my-14 space-y-8">
+      <div className="container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-6 my-14 space-y-8">
         {
           books.map((book) => <AllBooksCard key={book.id} book={book}></AllBooksCard>)
         }
--- src/app/loading.js
+++ src/app/loading.js
@@ -3,8 +3,7 @@ import React from 'react';
 
 const Loding = () => {
   return (
-    <span className="loading loading-dots loading-xl w-[20%] container mx-auto"></span>
-
+    <span className="loading loading-dots loading-xl w-[8%] block mx-auto"></span>
   );
 };
 
